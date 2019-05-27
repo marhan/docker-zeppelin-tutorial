642c197fea4d0694ee63bd36b3198712a6e7e986
Markus Hanses <me@markushanses.de>
2019-05-27 23:14:15 +0200

    Add dependencies while docker image build.

---
+++ zeppelin/docker/conf/zeppelin-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#export ZEPPELIN_MEM="-Xms1g -Xmx1g -XX:MaxPermSize=512m"
+#export ZEPPELIN_INTP_MEM="-Xms1g -Xmx1g -XX:MaxPermSize=512m"
+export ZEPPELIN_PORT=8080
+export ZEPPELIN_LOG_DIR='/opt/logs'
+export ZEPPELIN_NOTEBOOK_DIR='/opt/notebook'
+export SPARK_SUBMIT_OPTIONS="--jar $(JARS=("/opt/libs"/*.jar); IFS=,; echo "${JARS[*]}") --driver-class-path $(JARS=("/opt/libs"/*.jar); IFS=,; echo "${JARS[*]}") --properties-file /opt/properties/spark.properties"
+echo "############################################################"
+echo
+echo $SPARK_SUBMIT_OPTIONS
+echo
+echo "############################################################"
+
