b10a12f5cb0f7f0708b9a78301e6c150e980c980
Markus Hanses <markus.hanses@immonet.de>
2019-05-28 15:29:01 +0200

    Custom Spark binary integrated into Docker build. Not finished, yet.

--- zeppelin/docker/conf/zeppelin-env.sh
+++ zeppelin/docker/conf/zeppelin-env.sh
@@ -4,7 +4,7 @@
 export ZEPPELIN_PORT=8080
 export ZEPPELIN_LOG_DIR='/opt/logs'
 export ZEPPELIN_NOTEBOOK_DIR='/opt/notebook'
-export SPARK_SUBMIT_OPTIONS="--jar $(JARS=("/opt/libs"/*.jar); IFS=,; echo "${JARS[*]}") --driver-class-path $(JARS=("/opt/libs"/*.jar); IFS=,; echo "${JARS[*]}") --properties-file /opt/properties/spark.properties"
+export SPARK_SUBMIT_OPTIONS="--jar $(JARS=("/opt/custom_lib"/*.jar); IFS=,; echo "${JARS[*]}") --driver-class-path $(JARS=("/opt/custom_lib"/*.jar); IFS=,; echo "${JARS[*]}") --properties-file /opt/properties/spark.properties"
 echo "############################################################"
 echo
 echo $SPARK_SUBMIT_OPTIONS
