3362694f43ca097f41ff7285b94f933cc99093e9
Markus Hanses <me@markushanses.de>
2019-05-29 06:52:39 +0200

    Docker build process improved.

--- zeppelin/docker/conf/zeppelin-env.sh
+++ zeppelin/docker/conf/zeppelin-env.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+
 #export ZEPPELIN_MEM="-Xms1g -Xmx1g -XX:MaxPermSize=512m"
 #export ZEPPELIN_INTP_MEM="-Xms1g -Xmx1g -XX:MaxPermSize=512m"
+
 export ZEPPELIN_PORT=8080
 export ZEPPELIN_LOG_DIR='/opt/logs'
 export ZEPPELIN_NOTEBOOK_DIR='/opt/notebook'
-export SPARK_SUBMIT_OPTIONS="--jar $(JARS=("/opt/custom_lib"/*.jar); IFS=,; echo "${JARS[*]}") --driver-class-path $(JARS=("/opt/custom_lib"/*.jar); IFS=,; echo "${JARS[*]}") --properties-file /opt/properties/spark.properties"
+
+export CUSTOM_LIBS="/opt/custom_lib"
+export CUSTOM_PROPERTIES="/opt/properties"
+export SPARK_SUBMIT_OPTIONS="--jar $(JARS=("${CUSTOM_LIBS}"/*.jar); IFS=,; echo "${JARS[*]}") --driver-class-path $(JARS=("${CUSTOM_LIBS}"/*.jar); IFS=,; echo "${JARS[*]}") --properties-file ${CUSTOM_PROPERTIES}/spark.properties"
+
 echo "############################################################"
-echo
-echo $SPARK_SUBMIT_OPTIONS
+echo 
+echo  " Start with spark options: ${SPARK_SUBMIT_OPTIONS}"
 echo
 echo "############################################################"
 
--- zeppelin/load_libs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mvn dependency:list >> depenency.txt
-
-mvn dependency:copy-dependencies -DoutputDirectory=libs
