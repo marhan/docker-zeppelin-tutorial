82247a14ad522dafe5d6869107bdef18df9fca0f
Markus Hanses <me@markushanses.de>
2019-06-01 15:21:27 +0200

    S3 interaction finished.

--- zeppelin/docker/conf/zeppelin-env.sh
+++ zeppelin/docker/conf/zeppelin-env.sh
@@ -10,8 +10,14 @@ export ZEPPELIN_NOTEBOOK_DIR='/opt/notebook'
 export CUSTOM_LIBS="/opt/custom_lib"
 export CUSTOM_PROPERTIES="/opt/properties"
 
+export HADOOP_HOME="/opt/hadoop"
+export PATH=$PATH:$HADOOP_HOME/bin
+export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native
+
 export SPARK_HOME="/opt/spark"
 export PATH=$PATH:$SPARK_HOME/bin
+
+export SPARK_DIST_CLASSPATH=$(hadoop classpath)
 
 export SPARK_SUBMIT_OPTIONS="--jars $(JARS=("${CUSTOM_LIBS}"/*.jar); IFS=,; echo "${JARS[*]}") --properties-file ${CUSTOM_PROPERTIES}/spark.properties"
 
